5dcaf90525ddda88bcd888f67d9003236846173f
Ayussh Gupta <72377031+KingAyussh1@users.noreply.github.com>
2024-06-27 18:58:16 +0530

    Update ce.js

--- src/ce.js
+++ src/ce.js
@@ -1,1 +1,28 @@
+import React, { useState } from 'react';
+import Prism from 'prismjs';
+import "prismjs/themes/prism.css";
+import './CodeEditor.css';
+
+const CodeEditor = () => {
+  const [code, setCode] = useState('');
+
+  const handleChange = (event) => {
+    setCode(event.target.value);
+  };
+
+  const highlightedCode = Prism.highlight(code, Prism.languages.javascript, 'javascript');
+
+  return (
+    <div className="code-editor">
+      <textarea
+        value={code}
+        onChange={handleChange}
+        className="code-input"
+      />
+      <pre className="code-output" dangerouslySetInnerHTML={{ __html: highlightedCode }} />
+    </div>
+  );
+};
+
+export default CodeEditor;
 
